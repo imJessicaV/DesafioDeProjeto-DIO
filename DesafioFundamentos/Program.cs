0839d32cdcd29ae24a9df143a153de55a5320b13
Jéssica Vitoriano <103972128+imJessicaV@users.noreply.github.com>
2024-05-01 12:02:54 -0300

    Continuação dos estudos

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,61 +1,71 @@
-﻿using DesafioFundamentos.Models;
+﻿using DesafioFundamentos;
+using DesafioFundamentos.Models;
+using Newtonsoft.Json;
 
-// Coloca o encoding para UTF8 para exibir acentuação
-Console.OutputEncoding = System.Text.Encoding.UTF8;
+Vendas V1 = new  Vendas(1, "Caderno Star Wars", 25.00M);
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
-DateTime registraHoraEntrada = DateTime.Now;
-DateTime registraHoraSaida = DateTime.Now;
+string serializado = JsonConvert.SerializeObject(V1, Formatting.Indented); //serialização do objeto
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+File.WriteAllText("Arquivos/vendas.json", serializado); //transforma o objeto serializado em um arqquivo com o conteudo do objeto serializado
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine(serializado);
 
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
-Estacionamento es = new Estacionamento(precoInicial, precoPorHora, registraHoraEntrada, registraHoraSaida);
+// // Coloca o encoding para UTF8 para exibir acentuação
+// Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-string opcao = string.Empty;
-bool exibirMenu = true;
+// decimal precoInicial = 0;
+// decimal precoPorHora = 0;
+// DateTime registraHoraEntrada = DateTime.Now;
+// DateTime registraHoraSaida = DateTime.Now;
 
-// Realiza o loop do menu
-while (exibirMenu)
-{
-    Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+// Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+//                   "Digite o preço inicial:");
+// precoInicial = Convert.ToDecimal(Console.ReadLine());
 
-    switch (Console.ReadLine())
-    {
-        case "1":
-            es.AdicionarVeiculo();
-            break;
+// Console.WriteLine("Agora digite o preço por hora:");
+// precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
-        case "2":
-            es.RemoverVeiculo();
-            break;
+// // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+// Estacionamento es = new Estacionamento(precoInicial, precoPorHora, registraHoraEntrada, registraHoraSaida);
 
-        case "3":
-            es.ListarVeiculos();
-            break;
+// string opcao = string.Empty;
+// bool exibirMenu = true;
 
-        case "4":
-            exibirMenu = false;
-            break;
+// // Realiza o loop do menu
+// while (exibirMenu)
+// {
+//     Console.Clear();
+//     Console.WriteLine("Digite a sua opção:");
+//     Console.WriteLine("1 - Cadastrar veículo");
+//     Console.WriteLine("2 - Remover veículo");
+//     Console.WriteLine("3 - Listar veículos");
+//     Console.WriteLine("4 - Encerrar");
 
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
-    }
+//     switch (Console.ReadLine())
+//     {
+//         case "1":
+//             es.AdicionarVeiculo();
+//             break;
 
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
-}
+//         case "2":
+//             es.RemoverVeiculo();
+//             break;
 
-Console.WriteLine("O programa se encerrou");
+//         case "3":
+//             es.ListarVeiculos();
+//             break;
+
+//         case "4":
+//             exibirMenu = false;
+//             break;
+
+//         default:
+//             Console.WriteLine("Opção inválida");
+//             break;
+//     }
+
+//     Console.WriteLine("Pressione uma tecla para continuar");
+//     Console.ReadLine();
+// }
+
+// Console.WriteLine("O programa se encerrou");
